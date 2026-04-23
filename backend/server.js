028757234c8db2e10a64e21aaeb6309ed128e8ac
HieuIT4532 <nguyenhieuit2009@gmail.com>
2026-04-23 22:35:12 +0700

    upd

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // API Routes (to be added)
 app.get('/api/health', (req, res) => {
